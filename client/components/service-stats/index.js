e59e153bb8f848dc07b8bfbbd3668513910fe738
Sam Flint <sam.flint@corvesta.com>
2018-09-05 14:38:37 -0400

    revert and remove this.props

--- client/components/service-stats/index.js
+++ client/components/service-stats/index.js
@@ -8,8 +8,8 @@ export default class ServiceStats extends Component {
   render() {
     const { service } = this.props;
     const { runningCount, desiredCount } = service;
-    const { image } = this.props.service.task.containerDefinitions[0].image.substring(0,this.props.service.task.containerDefinitions[0].image.indexOf(':'));
-    const { commit } = this.props.service.task.containerDefinitions[0].image.substring(this.props.service.task.containerDefinitions[0].image.indexOf(':')+1);
+    const { image } = service.task.containerDefinitions[0];
+    const { commit } = service.task.containerDefinitions[0].image.substring(this.props.service.task.containerDefinitions[0].image.indexOf(':')+1);
     const date = moment(service.deployments[0].updatedAt);
     const updatedAgo = date.fromNow();
     const updatedIso = date.toISOString();
@@ -24,12 +24,10 @@ export default class ServiceStats extends Component {
             <tr>
               <th>Image</th>
               <td title={image}>{image}</td>
-              <td title={image}>this.props.service.task.containerDefinitions[0].image.substring(0,this.props.service.task.containerDefinitions[0].image.indexOf(':'))</td>
             </tr>
             <tr>
               <th>Commit</th>
               <td title={commit}>{commit}</td>
-              <td title={commit}>this.props.service.task.containerDefinitions[0].image.substring(this.props.service.task.containerDefinitions[0].image.indexOf(':')+1)</td>
             </tr>
             <tr>
               <th>Running</th>
