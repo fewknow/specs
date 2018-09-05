9a56a043991c8b4327bff5246ef83d8cb190bc7a
Sam Flint <sam.flint@corvesta.com>
2018-09-05 14:06:20 -0400

    attemtping again

--- client/components/service-stats/index.js
+++ client/components/service-stats/index.js
@@ -8,9 +8,9 @@ export default class ServiceStats extends Component {
   render() {
     const { service } = this.props;
     const { runningCount, desiredCount } = service;
-    const { image } = service.task.containerDefinitions[0];
-    const { commit } = this.props.service.task.containerDefinitions[0].image.substring(this.props.service.task.containerDefinitions[0].image.indexOf(':')+1)
-    const date = moment(service.deployments[0].updatedAt)
+    const { image } = this.props.service.task.containerDefinitions[0].image.substring(0,this.props.service.task.containerDefinitions[0].image.indexOf(':'));
+    const { commit } = this.props.service.task.containerDefinitions[0].image.substring(this.props.service.task.containerDefinitions[0].image.indexOf(':')+1);
+    const date = moment(service.deployments[0].updatedAt);
     const updatedAgo = date.fromNow();
     const updatedIso = date.toISOString();
     const classes = classname({
@@ -24,10 +24,12 @@ export default class ServiceStats extends Component {
             <tr>
               <th>Image</th>
               <td title={image}>{image}</td>
+              <td title={image}>this.props.service.task.containerDefinitions[0].image.substring(0,this.props.service.task.containerDefinitions[0].image.indexOf(':'))</td>
             </tr>
             <tr>
               <th>Commit</th>
               <td title={commit}>{commit}</td>
+              <td title={commit}>this.props.service.task.containerDefinitions[0].image.substring(this.props.service.task.containerDefinitions[0].image.indexOf(':')+1)</td>
             </tr>
             <tr>
               <th>Running</th>
